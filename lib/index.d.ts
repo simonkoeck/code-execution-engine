215a129777ae6b88817161e9d20705d35da7b47d
Simon Köck <simonkoeck04@gmail.com>
2021-03-27 13:12:34 +0100

    fixed untracked files

--- lib/index.d.ts
+++
@@ -1,5 +0,0 @@
-import executer from "./executor";
-import l from "./languages";
-export declare const languages: typeof l;
-export declare const execute: typeof executer;
-//# sourceMappingURL=index.d.ts.map
