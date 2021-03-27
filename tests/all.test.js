977b8f6d4a3cbced8999940e996cfa2a56327463
Simon Köck <simonkoeck04@gmail.com>
2021-03-27 15:38:30 +0100

    change tests

--- tests/all.test.js
+++ tests/all.test.js
@@ -3,7 +3,7 @@ var cee = require("../lib/index");
 describe("Testing Basic Languages", () => {
   test("Bash", () => {
     return cee
-      .execute("echo 123", cee.languages.BASH, { security: { useLXC: true } })
+      .execute("echo 123", cee.languages.BASH, { security: { useLXC: false } })
       .then((response) => {
         expect(response).toBe("123");
       });
